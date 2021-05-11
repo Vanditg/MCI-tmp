6604963ea1d5ff997f90a0f3415da975cad85589
Vandit Gajjar <gajjarvandit@gmail.com>
2021-05-11 16:52:55 +0930

    Update app.py

--- app.py
+++ app.py
@@ -25,58 +25,8 @@ from dash.dependencies import Output, Input
 app = dash.Dash(__name__)
 server = app.server
 
-#Creating dataframe
-df = pd.read_csv("https://github.com/Vanditg/MCI-tmp/blob/main/datasheet_1.csv")
-
-
-#Extracting different datapoints
-SampleNames = df["SampleNames"].tolist()
-
-Gen0 = df["gen0"].tolist()
-Gen1 = df["gen1"].tolist()
-Gen2 = df["gen2"].tolist()
-Gen3 = df["gen3"].tolist()
-Gen4 = df["gen4"].tolist()
-Gen5 = df["gen5"].tolist()
-Gen6 = df["gen6"].tolist()
-Gen7 = df["gen7"].tolist()
-Gen8 = df["gen8"].tolist()
-
-totalCells = df["TotalCells"].tolist()
-
-timeHrs = df["TimeHrs"].tolist()
-
-#Creating application using Python-dash
-#app = dash.Dash(__name__)
-
-#Creating layout
-app.layout = html.Div([
-    html.H1("Modelling Immune Response in Python"),
-    dcc.Dropdown(id='sample-names',
-                 options=[{'label': x, 'value': x}
-                          for x in sorted(df.SampleNames.unique())],
-                 value='0.19'
-                 ),
-    dcc.Graph(id='my-graph', figure={}
-              )
-])
-
-#Making callbacks
-@app.callback(
-    Output(component_id='my-graph', component_property='figure'),
-    Input(component_id='sample-names', component_property='value')
-)
-
-#Defining function for interaction
-def interactive_graphs(sample_names_choice):
-    #print(time_hrs_choice)
-    dff = df[df.SampleNames == sample_names_choice]
-
-    fig = px.box(data_frame=dff[0:135], x='TimeHrs', y='TotalCells', title="TimeHrs Stamp vs. Total Number of Cells for different SampleNames Chart", points="all")
-
-    #fig.update_yaxes(range=[0, 50000])
-    return fig
-
+print('Hello')
+      
 #Main method
 if __name__=='__main__':
     app.run_server(debug=True)
